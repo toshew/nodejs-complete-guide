60c1fa9c4150b6ba57f9c6aaa71ea8fe885ef396
vladantosev <vladantosev@yahoo.com>
2020-02-09 17:53:27 +0100

    Implementing Pug templating engine for rendering views

--- routes/shop.js
+++ routes/shop.js
@@ -5,9 +5,11 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
